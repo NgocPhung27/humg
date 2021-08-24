31674ce477df72d297655076c979fef5c57883ef
PHUNG NHU NGOC <1721050229@student.humg.edu.vn>
2021-08-24 14:31:38 +0700

    2

---
+++ BTH2/BTH2/Controllers/BTH2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BTH2.Models;
+
+namespace BTH2.Controllers
+{
+    public class BTH2Controller : Controller
+    {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: BTTH2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1( double hesoA, double hesoB)
+        {
+            double x = gpt.GiaiPhuongTrinhBacNhat(hesoA, hesoB);
+            ViewBag.nghiemPT = x;
+            return View();
+
+        }
+
+    }
+}
--- BTH2/BTH2/Controllers/HomeController.cs
+++ BTH2/BTH2/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace BTH2.Controllers
 
             return View();
         }
+        public ActionResult Demo
     }
 }
