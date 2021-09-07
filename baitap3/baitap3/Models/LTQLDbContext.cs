8be58494b44afc349b74a6bd90e70c5cf050d843
PHUNG NHU NGOC <1721050229@student.humg.edu.vn>
2021-09-07 14:34:59 +0700

    1

---
+++ baitap3/baitap3/Models/LTQLDbContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace baitap3.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext()
+            : base("name=LTQLDbContext")
+        {
+        }
+            public virtual DbSet<Student> Students { get; set; }
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Student>()
+                .Property(e => e.StudentID)
+                .IsUnicode(false);
+                modelBuilder.Entity<Student>()
+                .Property(e => e.StudentName)
+                .IsUnicode(false);
+
+            }
+        }
+
+
+    }
+
---
+++ baitap3/baitap3/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace baitap3.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        public int StudentName { get; set; }
+    }
+}
