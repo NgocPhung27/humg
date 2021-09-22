743e65edcc69e881fb02a4f80e836234922c53b3
PHUNG NHU NGOC <1721050229@student.humg.edu.vn>
2021-09-23 00:01:41 +0700

    mit

---
+++ LapTrinhQuanLyDb/LapTrinhQuanLyDb/Controllers/EmpNewController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LapTrinhQuanLyDb.Models;
+
+namespace LapTrinhQuanLyDb.Controllers
+{
+    public class EmpNewController : Controller
+    {
+        LTQLDbContext db = new LTQLDbContext();
+        AutoGenerateKey genKey = new AutoGenerateKey();
+        // GET: EmpNew
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+        public ActionResult Create()
+        {
+            var empID = "";
+            var countEmployee = db.Employees.Count();
+            if (countEmployee == 0)
+            {
+                empID = "EMP001";
+            }
+            else
+            {
+                //Lấy giá trị PersonID moi nhat
+                var PersonID = db.Employees.ToList().OrderByDescending(m => m.PersonID).FirstOrDefault().PersonID;
+                //sinh PersonID tự dộng
+                empID = genKey.Generatekey(PersonID);
+            }
+            ViewBag.PersonID = empID;
+                return View();
+        }
+        [HttpPost]
+        public ActionResult Create (Employee emp)
+            
+        {
+           var countEmployee = db.Employees.Count();
+            if (countEmployee == 0)
+            {
+                emp.PersonID = "EMP001";
+            }
+            else
+            {
+                //Lấy giá trị PersonID moi nhat
+                var PersonID = db.Employees.ToList().OrderByDescending(m => m.PersonID).FirstOrDefault().PersonID;
+                //sinh PersonID tự dộng
+                emp.PersonID = genKey.Generatekey(PersonID);
+            }
+            //luu thông tin vao database
+            db.Employees.Add(emp);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LapTrinhQuanLyDb/LapTrinhQuanLyDb/Models/AutoGenerateKey.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace LapTrinhQuanLyDb.Models
+{
+    public class AutoGenerateKey
+    {
+        public string Generatekey(string id)
+        {
+            string strkey = "";
+            string numpPart = "", 
+            strPart = "", 
+            strPhanSo = "";
+            numpPart = Regex.Match(id, @"\d+").Value;
+            strPart = Regex.Match(id, @"\D+").Value;
+
+            int PhanSo = (Convert.ToInt32(numpPart) + 1);
+            for (int i = 0; i < numpPart.Length - PhanSo.ToString().Length; i++)
+            {
+                strPhanSo += "0";
+            }
+            strPhanSo += PhanSo;
+            //tách phần chữ
+            strkey = strPart + strPhanSo;
+            return strkey;
+        }
+
+  
+    }
+}
